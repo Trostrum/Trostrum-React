44336ac9d8266e7869c32787b715770937be519c
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-02-23 18:36:36 +0100

    Fix a bit of design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export default function HomeNav() {
   }
 
   return (
-    <>
+    <div className={'full-navbar-wrapper'}>
       <div className='upper-nav-wrapper'>
         <div className='upper-nav'>
           <div className='left'>
@@ -47,6 +47,6 @@ export default function HomeNav() {
           </div>
         </div>
       </nav>
-    </>
+    </div>
   )
 }
