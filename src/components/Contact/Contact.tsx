8ad4024a04be4c4219d77b430aadcaa3c47d602f
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-03-02 21:28:30 +0545

    create contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,7 @@
-import { Button, TextareaAutosize, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
+import Container from './../Container/Container'
+import Heading from './../Heading/Heading'
+import { LocationCity, Smartphone, Mail } from '@material-ui/icons'
 import './Contact.scss'
 
 export default function Contact() {
@@ -17,30 +19,45 @@ export default function Contact() {
     setFunction(value)
   }
   return (
-    <section className='contact-section' id='contact'>
-      <div className='contact-wrapper'>
-        <div className='map'>
-          <iframe className='frame' title='map' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.6227873215635!2d85.2952679145381!3d27.69805123250833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19dd53a0664b%3A0x8d06e6d1666e9e5d!2sSawari%20Motors%20pvt.ltd.!5e0!3m2!1sen!2snp!4v1613715434203!5m2!1sen!2snp' style={{ border: 0 }} aria-hidden='false'/>
-        </div>
-        <div className='contact-form' onSubmit={onFormSubmit}>
-          <h2>Contact Us</h2>
-          <p>You can contact us in person at any working hour.</p>
-          <form className='contact'>
+    <div className='contact-wrapper'>
+      <Container>
+        <Heading heading="Contact Us" subHeading="Keep in Touch" />
+        <div className='contact'>
+          <form onSubmit={onFormSubmit}>
             <div className='input-wrapper'>
-              <TextField className='input' label='Full Name' id="outlined-basic" variant="outlined" value={name} required onChange={(e) => { handleInputChange(setName, e.target.value as string) }} />
+              <input type='text' placeholder='Name' className='input' />
             </div>
             <div className='input-wrapper'>
-              <TextField className='input' label='Email' id="outlined-basic" variant="outlined" value={email} required onChange={(e) => { handleInputChange(setEmail, e.target.value as string) }} />
+              <input type='email' placeholder='Email' className='input' />
             </div>
             <div className='input-wrapper'>
-              <TextareaAutosize className='input description' style={{ background: 'transparent' }} placeholder='Description' rowsMax={10} rowsMin={8} value={message} required onChange={(e) => { handleInputChange(setMessage, e.target.value as string) }} />
-            </div>
-            <div className='btn-wrapper'>
-              <Button disabled={btnState} type='submit' className='btn' variant='contained' size='large' color='primary'>Submit</Button>
+              <textarea placeholder='Message' rows={8} className='input' ></textarea>
             </div>
           </form>
+          <div className='contact-details'>
+            <div className='contact-item'>
+              <LocationCity className='icon' />
+              <div className='text-wrapper'>
+                <p>2651 Main Street, Suit 124</p>
+                <p>Seattle, WA, 98101</p>
+              </div>
+            </div>
+            <div className='contact-item'>
+              <Smartphone className='icon' />
+              <div className='text-wrapper'>
+                <p>0123456789</p>
+                <p>0345627891</p>
+              </div>
+            </div>
+            <div className='contact-item'>
+              <Mail className='icon' />
+              <div className='text-wrapper'>
+                <p>trostrum@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </Container>
+    </div>
   )
 }
