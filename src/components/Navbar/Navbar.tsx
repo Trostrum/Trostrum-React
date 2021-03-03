19ae731c6e923b5a7a1c67a713768e26f2e40db0
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-03-03 13:21:50 +0100

    Fix scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ export default function HomeNav() {
     if (isResponsive) {
       document.body.style.overflow = 'hidden'
     } else {
-      document.body.style.overflow = 'hidden'
+      document.body.style.overflow = 'overflow'
     }
   }, [isResponsive])
 
